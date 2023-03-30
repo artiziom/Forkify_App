eed61e322e39d1fac40b19fc8a8d8986ecd76ec7
artiziom <artur.kompala@gmail.com>
2023-03-30 21:34:00 +0200

    test

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,27 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function(){
+    try{
+      const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=038d72ab-1bf1-41bd-bfbd-158903f099a5')
+      const data = await res.json();
+      
+      if(!res.ok) throw new Error(`${data.message}`);
+      let {recipe} = data.data
+      recipe = {
+        id: recipe.id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        sourceUrl: recipe.source_url,
+        image: recipe.image_url,
+        servings: recipe.servings,
+        cookingTime: recipe.cooking_time,
+        ingredients: recipe.ingredients,
+      }
+      console.log(recipe);
+    }catch(err){
+
+    }
+}
+showRecipe();
